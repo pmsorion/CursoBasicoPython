12210ed28d78b3986d8b55e4de484fd27590ee20
Francisco Garcia <garciafran@hotmail.com>
2020-08-25 20:29:58 -0500

    No repetir codigo use funciones

---
+++ codigo_python/funciones.py
@@ -0,0 +1,32 @@
+# def imprimir_mesanje():
+#     print('Mensaje especial')
+#     print('aprendiendo a usar funciones')
+
+# imprimir_mesanje()
+# imprimir_mesanje()
+# imprimir_mesanje()
+# imprimir_mesanje()
+
+# def conversacion(mensaje):
+#     print('Hola')
+#     print('Como estas')
+#     print(mensaje)
+#     print('Adios')
+
+# opcion = int(input('elije opciones(1, 2, 3)'))
+# if opcion == 1:
+#     conversacion('Elegiste la opcion 1')
+# elif opcion == 2:
+#     conversacion('Elegiste la opcion 2')
+# elif opcion == 3:
+#     conversacion('Elegiste la opcion 3')
+# else:
+#     print('elige la opcion correctoa')
+
+def suma(a, b):
+    print('Suma de dos numeros')
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)
