f37284f3503dfa5490bb7b82f868da515813727a
Francisco Garcia <garciafran@hotmail.com>
2020-08-26 16:02:27 -0500

    Numeros Primos

---
+++ codigo_python/prueba_primalidad.py
@@ -0,0 +1,35 @@
+# def es_primo(numero):
+#     contador = 0
+
+#     for i in range(1, numero + 1):
+#         if i == 1 or i == numero:
+#             continue
+#         if numero % i == 0:
+#             contador += 1
+#     if contador == 0:
+#         return True
+#     else:
+#         return False
+
+def es_primo(numero):
+    if numero < 1:
+        return False
+    elif numero == 2:
+        return True
+    else:
+        for i in range(2, numero):
+            if numero % i == 0:
+                return False
+        return True
+        
+
+def run():
+    numero = int(input('Escribe un numero: '))
+    if es_primo(numero):
+        print('Es primo')
+    else:
+        print('No es primo')
+
+
+if __name__ == "__main__":
+    run()
